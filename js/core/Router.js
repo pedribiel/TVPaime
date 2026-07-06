c8e0acd47fa41cea6ce461348ebe3b4e0423f280
pedribiel <ranibeiro@gmail.com>
2026-07-06 20:30:03 -0300

    Update Router.js

--- js/core/Router.js
+++ js/core/Router.js
@@ -1,7 +1,7 @@
 import { render } from "./Render.js";
 
 import { Home } from "../pages/Home.js";
-import { SearchPage } from "../pages/SearchBar.js";
+import { SearchBar } from "../pages/SearchBar.js";
 import { Favorites } from "../pages/Favorites.js";
 import { Profile } from "../pages/Profile.js";
 
@@ -11,7 +11,7 @@ export const Router = {
 
         home:Home,
 
-        search:SearchPage,
+        searchbar:SearchBar,
 
         favorites:Favorites,
 
