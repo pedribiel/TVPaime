d24a905928610ac6900fb4c89e23edc72426d927
pedribiel <ranibeiro@gmail.com>
2026-07-07 17:59:43 -0300

    Update App.js

--- js/core/App.js
+++ js/core/App.js
@@ -1,13 +1,31 @@
+import { Router } from "./Router.js";
 import { TMDB } from "../services/tmdb.js";
+import { setCatalog } from "../data/catalog.js";
 
 export const App = {
 
     async start(){
 
-        const data = await TMDB.trending();
+        try{
 
-        console.log(data);
+            const response = await TMDB.trending();
+
+            setCatalog(response.results);
+
+            console.log("Catálogo carregado!");
+
+            console.log(response.results);
+
+            Router.go("home");
+
+        }
+
+        catch(error){
+
+            console.error(error);
+
+        }
 
     }
 
-}
+};
