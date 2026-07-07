948e8d889f74302a37487d59cc9e46cad3ca5aee
pedribiel <ranibeiro@gmail.com>
2026-07-06 21:25:46 -0300

    Teste
    Teste

--- js/core/Router.js
+++ js/core/Router.js
@@ -1,8 +1,31 @@
+import { render } from "./Render.js";
+
+import { Home } from "../pages/Home.js";
+import { SearchPage } from "../pages/Search.js";
+import { Favorites } from "../pages/Favorites.js";
+import { Profile } from "../pages/Profile.js";
+
+export const Router = {
+
+    routes:{
+
+        home:Home,
+
+        search:SearchPage,
+
+        favorites:Favorites,
+
+        profile:Profile
+
+    },
+
 go(route){
 
     console.log("Rota:", route);
     console.log(this.routes);
 
     render(this.routes[route]);
+
+}
 
 }
