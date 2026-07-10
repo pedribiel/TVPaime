c5d259984473f4a3fbaafce0d14006da43c7dd83
pedribiel <ranibeiro@gmail.com>
2026-07-10 20:58:48 -0300

    Arrumando

--- js/core/Render.js
+++ js/core/Render.js
@@ -1,33 +1,11 @@
-import { Router } from "./Router.js";
+import { UI } from "./UI.js";
 
-export function render(page){
+export function render(page) {
 
     const app = document.getElementById("app");
 
     app.innerHTML = page();
 
-    document
-        .querySelectorAll("[data-route]")
-        .forEach(button=>{
-
-            button.onclick=()=>{
-
-                Router.go(
-
-                    button.dataset.route
-
-                );
-
-            };
-
-        });
-    
-    import { UI } from "./UI.js";
-
-    export function render(page){
-
-    document.getElementById("app").innerHTML = page();
-
     UI.init();
 
-}}
+}
