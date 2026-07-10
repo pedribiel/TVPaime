b1cf102431f796031905a3a91ac404ed34ef6925
pedribiel <ranibeiro@gmail.com>
2026-07-10 20:14:37 -0300

    Update watching.js

--- js/data/watching.js
+++ js/data/watching.js
@@ -1,1 +1,13 @@
-export let watching = [];
+import { Storage } from "../services/storage.js";
+
+export let watching = Storage.get("watching");
+
+export function addWatching(item){
+
+    if(watching.some(x=>x.id===item.id)) return;
+
+    watching.push(item);
+
+    Storage.set("watching",watching);
+
+}
