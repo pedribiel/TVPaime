bc7a5c61b1fe723b8bc482eb4fedd2e555b23ba7
pedribiel <ranibeiro@gmail.com>
2026-07-10 20:35:01 -0300

    Testes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 import { App } from "./core/App.js";
 
 App.start();
+
+import { setStatus, library } from "./data/library.js";
+import { catalog } from "./data/catalog.js";
+
+setTimeout(() => {
+
+    if (catalog.trending.length > 0) {
+
+        setStatus(catalog.trending[0], "watching");
+
+        console.log(library);
+
+    }
+
+}, 2000);
