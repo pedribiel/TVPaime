47508b64c5de16b0cd9761c20e52da45644fe066
pedribiel <ranibeiro@gmail.com>
2026-07-10 20:15:24 -0300

    Update watchlist.js

--- js/data/watchlist.js
+++ js/data/watchlist.js
@@ -1,1 +1,13 @@
-export let watchlist = [];
+import { Storage } from "../services/storage.js";
+
+export let watchlist = Storage.get("watchlist");
+
+export function addWatchlist(item){
+
+    if(watchlist.some(x=>x.id===item.id)) return;
+
+    watchlist.push(item);
+
+    Storage.set("watchlist",watchlist);
+
+}
