c51fb0f41edbd18e3ed512cd081c59b2d77254fa
pedribiel <ranibeiro@gmail.com>
2026-07-10 20:15:40 -0300

    Update completed.js

--- js/data/completed.js
+++ js/data/completed.js
@@ -1,1 +1,13 @@
-export let completed = [];
+import { Storage } from "../services/storage.js";
+
+export let completed = Storage.get("completed");
+
+export function addCompleted(item){
+
+    if(completed.some(x=>x.id===item.id)) return;
+
+    completed.push(item);
+
+    Storage.set("completed",completed);
+
+}
